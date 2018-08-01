4d4c117ae102672d1de8a21b62d6cc447bfee3b4
zhengshanlin <2410192547@qq.com>
2018-08-01 15:33:50 +0800

    learning progress

---
+++ 20180725/ArrayInRam2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class ArrayInRam2
+{
+	public static void main(String[] args)
+	{
+		//定义并初始化数组，使用静态初始化
+		int[] a={5,7,20};
+		//定义并初始化数组，使用动态初始化
+		int[] b=new int[4];
+		//输出b数组的长度
+		System.out.println("b数组的长度为："+b.length);
+		//循环输出a数组的元素
+		for (int i=0,len=a.length;i<len;i++)
+		{
+			System.out.println(a[i]);
+		}
+		//循环输出b数组的元素
+		for(int i=0,len=b.length;i<len;i++)
+		{
+			System.out.println(b[i]);
+		}
+		//因为a是int[]类型，b也是int[]类型，所以可以将a的值赋给b
+		//也就是让b引用指向a指向的数组
+		b=a;
+		//再次输出b数组的长度
+		System.out.println("b数组的长度为："+b.length);
+	}	
+}
+/*b数组的长度为：4
+5
+7
+20
+0
+0
+0
+0
+b数组的长度为：3*/
